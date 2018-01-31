71520e9e6d4dd9fe7bb2e0fdc2874e8b8ca249aa
hanzhichao <han_zhichao@sina.cn>
2018-01-31 21:03:04 +0800

    add concurrency and times/os.walk/format data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PostBoy---基于json文件的接口调试/测试工具
+# PostBoy---基于json文件数据驱动的接口测试框架
 
 ## Fixture
 * 接口数据基于json文件
@@ -32,7 +32,55 @@
   "remark":"根据用户ID获取用户信息"
 }
 ```
-
+需要替换参数的接口样例---指定fromat_data : true
+```
+{
+  "uri": "/api/ITakeaway/w",
+  "headers": {
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  "cookies": {
+    "PHPSESSID": "fd34e161b19433fb1cb39150cb5f17dc"
+  },
+  "concurrency": "5",
+  "times": "10",
+  "format_data": true,
+  "data": {
+    "params": "%s"
+  },
+  "_params": {
+    "address_id": "64948",
+    "delivery_date": "2018-01-18",
+    "start_time": "10:42:25",
+    "end_time": "11:42:25",
+    "is_reserve": "2",
+    "invoice_id": "0",
+    "money": "22.00",
+    "discount": "0",
+    "freight": "20.00",
+    "received": "42.00",
+    "is_invoice": "2",
+    "is_activity": "2",
+    "activity": "nothing",
+    "red_envelope": "nothing",
+    "pay_way": "2",
+    "pay_channel": "4",
+    "products": [
+      {
+        "id": "23",
+        "num": "1"
+      }
+    ],
+    "remark": "",
+    "user_scope": "1",
+    "channel": "14",
+    "card_amount": "0",
+    "card_id": "",
+    "user_id": "83",
+    "station_id": "53"
+  },
+}
+```
 
 ## Sublime Text  编译json接口，使用方法
 1. Sublime Text3 -> Tools -> Build System -> New Build System
@@ -46,4 +94,7 @@
 ```
 3. 在json接口文件窗口，选择编译系统为postboy(或自动)，按Ctrl+B 编译
 
+## 已知问题
+* json数据文件不支持注释
+* 不容易区分参数是否必选
 
---
+++ postboy.py
@@ -0,0 +1,195 @@
+#!/bin/env/python
+# coding=utf-8
+"""饿了么/美团/百度外卖 三方平台 测试/预发环境切换器"""
+import sys
+import json
+import requests
+import platform
+
+from threading import Thread
+import os
+from functools import wraps
+from sign import sign_params
+
+
+if (platform.python_version()) < '3':
+    import codecs
+    import ConfigParser
+else:
+    import configparser as ConfigParser
+
+
+def load_config(path, section='default'):
+    if (platform.python_version()) < '3':
+        conf = ConfigParser.ConfigParser()
+        with open(path, encoding='utf-8') as f:
+            conf.readfp(f)
+    else:
+        conf = ConfigParser.RawConfigParser()
+        conf.read(path, encoding='utf8')
+
+    _dict = {}
+    for option in conf.options(section):
+        _dict[option] = conf.get(section, option)
+    return _dict
+
+
+def load_json(path):
+    """加载json文件,:param path: json文件路径 :return 字典格式"""
+    if (platform.python_version()) < '3':
+        with codes.open(path, encoding='utf-8') as f:
+            try:
+                return json.load(f)
+            except json.decoder.JSONDecodeError:
+                print("%s --- json decode error" % path)
+
+    else:
+        with open(path, encoding='utf-8') as f:
+            try:
+                return json.load(f)
+            except json.decoder.JSONDecodeError:
+                print("%s --- json decode error" % path)
+
+
+def login():
+    session = requests.Session()
+    config = load_json(os.path.join(os.path.dirname(__file__), 'login.json'))
+    res = session.post(config.get('url'), headers=config.get('headers'), data=config.get('data'))
+    return session
+
+
+def threads(concurrency, times):
+    def _threads(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # print("start:%s" % time.ctime())
+            for i in range(0, times//concurrency):
+                threads = []
+                for i in range(concurrency):
+                    if not args or kwargs:
+                        t = Thread(target=func)
+                    elif args:
+                        t = Thread(target=func, args=args)
+                    elif kwargs:
+                        t = Thread(target=func, args=kwargs)
+                    threads.append(t)
+                for i in range(concurrency):
+                    threads[i].start()
+                for i in range(concurrency):
+                    threads[i].join()
+            # print("end:%s" % time.ctime())
+            return func
+        return wrapper
+    return _threads
+
+
+class PostBoy(object):
+    def __init__(self, config_file=os.path.join(os.path.dirname(__file__), 'default.conf')):
+        self.config = load_config(config_file)
+        self.params = {}
+
+
+    def read_api(self, api_file):
+        api = load_json(api_file)
+
+        if not api.get('url'):  # todo try ... except ...
+            if not base_url:
+                base_url = self.config.get('base_url')
+            self.params['api_url'] = base_url + api['uri']
+        else:
+            self.params['api_url'] = api.get('url')
+
+        # format data
+        if api.get('format_data') is True:
+            for key in api['data'].keys():
+                api['data'][key] = api['data'][key] % json.dumps(api.get('_'+key))
+
+        # need sign or not
+        if api.get('sign'):
+            sign = api.get('sign')
+        else:
+            sign = json.loads(self.config.get('sign'))
+
+        if sign:
+            accessId = sign.get('accessId')
+            accessKey = sign.get('accessKey')
+            self.params['data'] = sign_params(accessId,accessKey,api['data'])
+        else:
+            self.params['data'] = api['data']
+
+        # headers
+        if api.get('headers'):
+            headers = api.get('headers')
+        else:
+            headers = json.loads(self.config.get('headers'))
+        if "application/json" in headers.get('Content-Type'):  # must be Content-Type
+            self.params['data'] = json.dumps(self.params['data'])
+
+        # if is login_required
+        if api.get('login_required') is not None:
+            self.params['login_required'] = api.get('login_required')
+        else:
+            self.params['login_required'] = False if self.config.get('login_required')=='False' else True
+
+
+        # handle cookies
+        if api.get('cookies'):
+            self.params['cookies'] = api.get('cookies')
+
+
+        # handle concurrency and times
+        if api.get('concurrency') and api.get('times'):
+            self.params['concurrency'] = int(api.get('concurrency'))
+            self.params['times'] = int(api.get('times'))
+        else:
+            self.params['concurrency'] = int(self.config.get('concurrency'))
+            self.params['times'] = int(self.config.get('concurrency'))
+
+
+    def post(self, api_file):
+        self.read_api(api_file)
+
+        @threads(self.params.get('concurrency'), self.params.get('times'))
+        def _post():
+            if self.params.get('login_required'):
+                session = login()
+            else:
+                session = requests.Session()
+            
+            # post request
+            res = session.post(self.params.get('api_url'), 
+                headers=self.params.get('headers'), 
+                cookies=self.params.get('cookies'), 
+                data=self.params.get('data'))
+
+            # print(res.text)
+            if '"code":100000' in res.text:
+                print("%s ------ PASS" % api_file)
+            else:
+                print("%s ------ FAIL" % api_file)
+
+        _post()
+                                                
+
+
+def main():
+    if len(sys.argv) > 1:
+        # print(len(sys.argv))                              
+        postboy = PostBoy()
+        if os.path.isdir(sys.argv[1]):
+            for root, dirs, files in os.walk(sys.argv[1], topdown=False):
+                for name in files:
+                    if name[-5:] == '.json':
+                        # print(os.path.join(root, name))
+                        postboy.post(os.path.join(root, name))
+
+        elif os.path.isfile(sys.argv[1]):
+            postboy.post(sys.argv[1])
+        else:
+            print("文件不存在")
+        # postboy.post(sys.argv[1]) 
+
+# if __name__ != '__main__':
+main()
+# post('api/user/getInfoById') 
+# post('api/getGoodsCode.json', 'http://detail.spicespirit.com') 
